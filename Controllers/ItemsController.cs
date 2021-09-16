20a1ffd6e0b0bb5a38cd3a0d8c6097ac667aa434
chald80 <72771464+chald80@users.noreply.github.com>
2021-09-16 11:30:36 +0200

    Add project files.

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using testRest1.Managers;
+using testRest1.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace testRest1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly ItemsManager _manager = new ItemsManager();
+
+        // GET: api/<BooksController>
+        [HttpGet]
+        public IEnumerable<Item> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<BooksController>/5
+        [HttpGet("{id}")]
+        public Item Get(int id)
+        {
+            return _manager.GetById(id);
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public Item Post([FromBody] Item value)
+        {
+            return _manager.Add(value);
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{id}")]
+        public Item Put(int id, [FromBody] Item value)
+        {
+            return _manager.Update(id, value);
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{id}")]
+        public Item Delete(int id)
+        {
+            return _manager.Delete(id);
+        }
+    }
+}
---
+++ Managers/ItemsManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using testRest1.Models;
+
+namespace testRest1.Managers
+{
+    public class ItemsManager
+    {
+        private static int _nextId = 1;
+        private static readonly List<Item> Data = new List<Item>
+        {
+            new Item {Id = _nextId++, Name = "C# is nice", ItemQuality = 4, Quantity = 10},
+            new Item {Id=_nextId++, Name = "Python is even nicer", ItemQuality = 7, Quantity = 14}
+            // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/object-and-collection-initializers
+        };
+
+        public List<Item> GetAll()
+        {
+            return new List<Item>(Data);
+            // copy constructor
+            // Callers should no get a reference to the Data object, but rather get a copy
+        }
+
+        public Item GetById(int id)
+        {
+            return Data.Find(item => item.Id == id);
+        }
+
+        public Item Add(Item newItem)
+        {
+            newItem.Id = _nextId++;
+            Data.Add(newItem);
+            return newItem;
+        }
+
+        public Item Delete(int id)
+        {
+            Item item = Data.Find(item1 => item1.Id == id);
+            if (item == null) return null;
+            Data.Remove(item);
+            return item;
+        }
+
+        public Item Update(int id, Item updates)
+        {
+            Item item = Data.Find(item1 => item1.Id == id);
+            if (item == null) return null;
+            item.Name = updates.Name;
+            item.ItemQuality = updates.ItemQuality;
+            item.Quantity = updates.Quantity;
+            return item;
+        }
+    }
+}
